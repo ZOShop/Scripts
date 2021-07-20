98a0701bcd286a7b8d6686c3cd6f1e1c9605a313
ZOShop <87680803+ZOShop@users.noreply.github.com>
2021-07-19 23:35:04 -0300

    Create auth.lua

---
+++ auth.lua
@@ -0,0 +1,42 @@
+-- auth = false
+local ip = {}
+-- local zo = {}
+
+function zo:checkvalue (tab, val)
+    for index, value in ipairs(tab) do
+        if value.ip == val then
+            return true
+        end
+    end
+    
+    return false
+end
+
+PerformHttpRequest('ipv4bot.whatismyipaddress.com/', 
+    function(errorCode2, resultData2, resultHeaders2)
+        PerformHttpRequest('http://54.39.11.213:3000/ip/buscarips/' .. idUser,
+            function(errorCode, resultData, resultHeaders)
+                resultData = json.decode(resultData)
+
+                if resultData["ips"] ~= nil then
+                    ip = resultData2
+                    if zo:checkvalue(resultData["ips"], ip) then 
+                        auth = true
+                        zo:checkuth()
+                    else 
+                        zo:checkuth()
+                    end
+                else
+                    auth = true
+                    zo:checkuth()
+                end
+        end)
+end)
+
+-- function zo:checkuth()
+--     if auth then
+--         print(" ^2SCRIPT AUTENTICADO COM SUCESSO !^0")
+--     else
+--         print(" ^1SCRIPT NAO AUTENTICADO^0")
+--     end
+-- end
