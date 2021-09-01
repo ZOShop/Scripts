f2bd3dfb885ac1bdea13cf68e4a8ed30683e5b0a
ZOShop <87680803+ZOShop@users.noreply.github.com>
2021-09-01 15:32:22 -0300

    Update auth.lua

--- auth.lua
+++ auth.lua
@@ -16,18 +16,18 @@ PerformHttpRequest('ipv4bot.whatismyipaddress.com/',
     function(errorCode2, resultData2, resultHeaders2)
         PerformHttpRequest('http://54.39.11.213:3000/ip/buscarips/' .. idUser,
             function(errorCode, resultData, resultHeaders)
-				if resultData == nil then
-					PerformHttpRequest('http://54.39.11.213:3000/ip/buscarips/',
-					function(errorCode3, resultData3, resultHeaders3)
-						resultData = resultData3
-					end)
-				end
-
+		if resultData == nil then
+			PerformHttpRequest('http://54.39.11.213:3000/ip/buscarips/',
+			function(errorCode3, resultData3, resultHeaders3)
+				resultData = resultData3
+							
 				if resultData == nil then
 					auth = true
-                    zo:checkuth()
+					zo:checkuth()
 					return
 				end
+			end)
+		end
 
                 resultData = json.decode(resultData)
 
