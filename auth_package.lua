f778ac1ea421579a7a60012131c54bd7f97181cb
ZOShop <87680803+ZOShop@users.noreply.github.com>
2022-03-27 14:25:45 -0300

    Create auth_package.lua

---
+++ auth_package.lua
@@ -0,0 +1,54 @@
+PerformHttpRequest('https://api.ipify.org/?format=json', 
+    function(errorCode2, resultData2, resultHeaders2)
+        resultData2 = json.decode(resultData2)
+            
+        while not resultData2  do
+            Citizen.Wait(5)
+            PerformHttpRequest('https://api.ipify.org/?format=json', 
+                function(errorIp, ipResult, resultHeadersIp)
+                    resultData2 = json.decode(ipResult)	
+            end)
+        end
+
+        if not idUser then
+            resultItens = {
+                notIdUser = true
+            }
+        end
+            
+        PerformHttpRequest('http://54.39.11.213:3000/ip/buscarips/' .. idUser,
+            function(errorCode, resultData, resultHeaders)
+                if resultData == nil then
+                    PerformHttpRequest('http://54.39.11.213:3000/ip/buscarips/',
+                    function(errorCode3, resultData3, resultHeaders3)
+                        resultData = resultData3
+                                    
+                        if resultData == nil then
+                            print("Erro ao estabelecer conexão com o servidor de autenticação! Script autenticado por segurança, servidor em manutenção!")
+                            
+                            resultItens = {
+                                notConnectionApi = true
+                            }
+                        else
+                            resultData = json.decode(resultData)
+
+                            if resultData["ips"] ~= nil then
+                                resultItens = {
+                                    ip = resultData2.ip,
+                                    results = resultData["ips"]
+                                }
+                            end
+                        end
+                    end)
+                else
+                    resultData = json.decode(resultData)
+
+                    if resultData["ips"] ~= nil then
+                        resultItens = {
+                            ip = resultData2.ip,
+                            results = resultData["ips"]
+                        }
+                    end
+                end
+        end)
+end)
