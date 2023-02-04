77b3a332de9a18b7fc3bbc3ce53ab551ccd4b90a
Thiago Delanhese <55116380+Tdelanhese@users.noreply.github.com>
2023-02-03 21:55:47 -0300

    Update auth.lua

--- auth.lua
+++ auth.lua
@@ -1,25 +1,11 @@
 if not idUser then
     print("idUser não encontrado")
 else
-    PerformHttpRequest('https://api.ipify.org/?format=json', 
-        function(errorCode2, resultData2, resultHeaders2)
-            resultData2 = json.decode(resultData2)
-
-            while not resultData2 do
-                Citizen.Wait(5)
-
-                PerformHttpRequest('https://api.ipify.org/?format=json', 
-                    function(errorIp, ipResult, resultHeadersIp)
-                        resultData2 = json.decode(ipResult)	
-                end)
+    PerformHttpRequest('http://102.165.46.81:3000/ip/buscarips/' .. idUser,
+        function(errorCode, resultData, resultHeaders)
+            if resultData ~= nil then
+                print("AUTENTICADO COM SUCESSO")
+                auth = true
             end
-
-            PerformHttpRequest('http://54.39.11.213:3000/ip/buscarips/' .. idUser,
-                function(errorCode, resultData, resultHeaders)
-                    if resultData ~= nil then
-                        print("AUTENTICADO COM SUCESSO")
-                        auth = true
-                    end
-            end)
     end)
 end
