d72155ee27a16aa235fa6cf7a0f0a0ab92120595
Thiago Delanhese <55116380+Tdelanhese@users.noreply.github.com>
2024-01-18 10:55:10 -0300

    Update auth.lua

--- auth.lua
+++ auth.lua
@@ -1,7 +1,7 @@
 if not idUser then
     print("idUser não encontrado")
 else
-    PerformHttpRequest('http://191.96.225.149:3000/ip/buscarips/' .. idUser,
+    PerformHttpRequest('http://89.213.5.217:3000/ip/buscarips/' .. idUser,
         function(errorCode, resultData, resultHeaders)
             if resultData ~= nil then
                 auth = true
